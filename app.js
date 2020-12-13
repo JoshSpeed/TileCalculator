335cd22ee1a2ae4fc879900428906040bfd36701
Josh Speed <joshuabspeed@hotmail.co.uk>
2020-12-13 16:03:50 +0000

    Bug fix and price addition

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 //jshint esversion:6
 
-const submit = document.querySelector("button");
+const submit = document.querySelector(".button");
 const width = document.querySelector(".width");
 const length = document.querySelector(".length");
 const totalSize = document.querySelector(".total-size");
 const totalTiles = document.querySelector(".total-tiles");
 const totalRamps = document.querySelector(".total-ramps");
+const totalPrice = document.querySelector(".total-price");
+const tileForm = document.querySelector("#tile-form");
 
 const tiles = document.querySelectorAll(".grid-item");
 const tileChoice = document.querySelector(".selectedTile");
+
+const tilePrice = document.querySelector(".tile-price");
+const rampPrice = document.querySelector(".ramp-price");
 
 const popup = document.querySelector(".popup");
 const popupContent = document.querySelector(".popup-content");
@@ -20,62 +25,89 @@ tiles.forEach(function (i) {
         if (i.classList.contains("grid-1")) {
             tileChoice.value = "Black Raised Disk";
             popupContent.style.backgroundImage = 'url(/images/BlackRaisedDiskTileComp.jpg)';
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-2")) {
             tileChoice.value = "Graphite Raised Disk";
             popupContent.style.backgroundImage = "url(/images/GraphiteRaisedDiskComp.jpg)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-3")) {
             tileChoice.value = "Dark Blue Raised Disk";
             popupContent.style.backgroundImage = "url(/images/RaisedDiskDarkBlueComp.jpg)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-4")) {
             tileChoice.value = "Dark Grey Raised Disk";
             popupContent.style.backgroundImage = "url(/images/RaisedDiskDarkGreyComp.jpg)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-5")) {
             tileChoice.value = "Light Grey Raised Disk";
             popupContent.style.backgroundImage = "url(/images/RaisedDiskLightGreyComp)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-6")) {
             tileChoice.value = "Smooth Black";
             popupContent.style.backgroundImage = "url(/images/SmoothBlackComp.jpg)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-7")) {
             tileChoice.value = "Smooth Dark Blue";
             popupContent.style.backgroundImage = "url(/images/SmoothDarkBlueComp.jpg)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-8")) {
             tileChoice.value = "Smooth Dark Grey";
             popupContent.style.backgroundImage = "url(/images/SmoothDarkGreyComp.jpg)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-9")) {
             tileChoice.value = "Smooth Graphite";
             popupContent.style.backgroundImage = "url(/images/SmoothGraphiteComp.jpg)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-10")) {
             tileChoice.value = "Smooth Green";
             popupContent.style.backgroundImage = "url(/images/SmoothGreenComp.jpg)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-11")) {
             tileChoice.value = "Smooth Light Grey";
             popupContent.style.backgroundImage = "url(/images/SmoothLightGreyComp.jpg)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-12")) {
             tileChoice.value = "Smooth Red";
             popupContent.style.backgroundImage = "url(/images/SmoothRedComp.jpg)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
         if (i.classList.contains("grid-13")) {
             tileChoice.value = "Smooth Yellow";
             popupContent.style.backgroundImage = "url(/images/SmoothYellowComp.jpg)";
+            tilePrice.value = "10";
+            rampPrice.value = "5";
         }
 
     });
 });
 
+
 previewBtn.addEventListener("click", function () {
 
-    if (tileChoice.value === "") {
+    if (popupContent.style.backgroundImage === "") {
         return;
     } else {
         popup.classList.remove("hidden");
@@ -87,7 +119,8 @@ popup.addEventListener("click", function () {
 });
 
 
-submit.addEventListener("click", function () {
+submit.addEventListener("click", function (e) {
+
 
     let widthTile = Number(width.value);
     let lengthTile = Number(length.value);
@@ -95,4 +128,10 @@ submit.addEventListener("click", function () {
     totalSize.innerText = (widthTile * lengthTile);
     totalTiles.innerText = Math.ceil(totalSize.innerText * 4);
     totalRamps.innerText = Math.ceil(widthTile * 2);
+
+    let tilesNum = totalTiles.innerText;
+    let rampsNum = totalRamps.innerText;
+
+    totalPrice.innerText = ((tilesNum * Number(tilePrice.value)) + (rampsNum * Number(rampPrice.value)));
+
 });
